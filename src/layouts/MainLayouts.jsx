b142ef26d7f0d62461c38e7744f92e85dcfa4e2a
mickelj137kilawp-pixel <mickelj137kilawp@gmail.com>
2026-03-09 13:21:18 +0700

    Add files via upload

---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../component/Navbar";
+import Footer from "../components/Footer";
+export default function MainLayout({ children }) {
+return (
+<>
+<Navbar />
+{children}
+<Footer />
+</>
+);
+}
---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+export default function About() { 
+return ( 
+<section className="section"> 
+<h2>Tentang Saya</h2> 
+<p> 
+Saya adalah siswa kelas XI Rekayasa 
+Perangkat Lunak yang memiliki minat 
+di bidang pemrograman web dan pengembangan 
+aplikasi. 
+</p> 
+<p>Nama:Michael Nidjia Perdana Malau </p>
+<p>Kelas: XI RPL </p>
+<p>Sekolah: SMK PUSDIKHUB  </p>
+</section> 
+  ); 
+}
